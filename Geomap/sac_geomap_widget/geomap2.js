a6bb3393482f9550901563ea772b2ab709245116
Szymon Sanocki <szymon.sanocki@gds.ey.com>
2021-12-23 14:01:34 +0100

    SAC widger - switch to Mapbox

--- Geomap/sac_geomap_widget/geomap2.js
+++ Geomap/sac_geomap_widget/geomap2.js
@@ -1,262 +1,256 @@
 (function () {
 
-    LoadScript();
-    function LoadScript() {
-        console.log("Load script funcation");
-        const script2 = document.createElement('script');
-        script2.type = 'text/javascript';
-        script2.src = 'https://cdn.jsdelivr.net/npm/dat.gui@0.7.6/build/dat.gui.min.js';
-        document.head.appendChild(script2);
-        eval(script2);
+    loadScript();
+
+    function loadScript() {
+
+        console.log("Load script function");
 
         const script1 = document.createElement('script');
         script1.type = 'text/javascript';
-        script1.src = 'https://cdn.jsdelivr.net/npm/gcoord@0.2.3/dist/gcoord.js';
+        script1.src = 'https://d3js.org/d3.v5.min.js';
         document.head.appendChild(script1);
         eval(script1);
 
+        const script2 = document.createElement('script');
+        script2.type = 'text/javascript';
+        script2.src = 'https://api.mapbox.com/mapbox-gl-js/v2.6.0/mapbox-gl.js';
+        document.head.appendChild(script2);
+        eval(script2);
+
+        const script3 = document.createElement('script');
+        script3.type = 'text/javascript';
+        script3.src = 'https://unpkg.com/@turf/turf@6/turf.min.js';
+        document.head.appendChild(script3);
+        eval(script3);
+
         const script4 = document.createElement('script');
         script4.type = 'text/javascript';
-        script4.src = 'https://maptalks.org/maptalks.three/demo/js/maptalks.js';
+        script4.src = 'https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v4.7.2/mapbox-gl-geocoder.min.js';
         document.head.appendChild(script4);
         eval(script4);
 
-        const script5 = document.createElement('script');
-        script5.type = 'text/javascript';
-        script5.src = 'https://cdn.jsdelivr.net/npm/three@0.104.0/build/three.min.js';
-        document.head.appendChild(script5);
-        eval(script5);
-
-        setTimeout(function () {
-            const script6 = document.createElement('script');
-            script6.type = 'text/javascript';
-            script6.src = 'https://cdn.jsdelivr.net/npm/maptalks.three@latest/dist/maptalks.three.js';
-            document.head.appendChild(script6);
-            eval(script6);
-        }, 2000);
-
-        const script7 = document.createElement('script');
-        script7.type = 'text/javascript';
-        script7.src = 'https://cdn.jsdelivr.net/npm/three@0.104.0/examples/js/libs/stats.min.js';
-        document.head.appendChild(script7);
-        eval(script7);
     }
 
     let template = document.createElement("template");
+
     template.innerHTML = `
         <style>
-                   @import "https://cdn.jsdelivr.net/npm/maptalks/dist/maptalks.css";
-            html,
-            body {
-                margin: 0px;
-                height: 100%;
-                width: 100%;
-            }
-            #map {
-                width: 100%;
-                height: 100%;
-                background-color: #b2c2d2
-            }
-         .maptalks-attribution{
-            display:none;
-          }
+            @import "https://api.mapbox.com/mapbox-gl-js/v2.6.0/mapbox-gl.css";
+            @import "https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v4.7.2/mapbox-gl-geocoder.css";
             
+            body {
+                margin: 0;
+                padding: 0;
+                font-family: Helvetica, Arial, sans-serif;
+              }
+          
+              #map {
+                position: absolute;
+                top: 0;
+                bottom: 0;
+                width: 100%;
+              }
+          
+              #control-panel {
+                position: absolute;
+                background: #fff;
+                top: 0;
+                left: 0;
+                margin: 12px;
+                padding: 20px;
+                font-size: 12px;
+                line-height: 1.5;
+                z-index: 1;
+              }
+          
+              h2 {
+                padding: 0px;
+                margin: 0px;
+              }
+          
+              label {
+                display: inline-block;
+                width: 140px;
+              }          
         </style>
-      <div id="map"></div>
+
+        <body>
+        <div id="map"></div>
+        </body>
     `;
 
-    var map = "";
-    var materials = {};
-
     function load(prop, ele, cent) {
+
         let cen = [];
         cen[0] = parseFloat(cent.split(',')[0]);
         cen[1] = parseFloat(cent.split(',')[1]);
 
-        if (map != "") {
-            map.remove();
-        }
-        map = new maptalks.Map(ele, {
-            "center": cen,
-            zoom: 12,
-            pitch: 0,
-            dragPitch: true,
-            //allow map to drag rotating, true by default
-            dragRotate: true,
-            //enable map to drag pitching and rotating at the same time, false by default
-            dragRotatePitch: true,
-            baseLayer: new maptalks.TileLayer('tile', {
-                urlTemplate: 'http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png',
-                subdomains: ['a', 'b', 'c', 'd', 'e'],
-            })
-        });
-        // the ThreeLayer to draw buildings
-        threeLayer = new maptalks.ThreeLayer('t', {
-            forceRenderOnMoving: true,
-            forceRenderOnRotating: true
-            // animation: true
-        });
-        threeLayer.prepareToDraw = function (gl, scene, camera) {
-            stats = new Stats();
-            stats.domElement.style.zIndex = 100;
-            //document.getElementById('Geomap').appendChild(stats.domElement);
-            ele.appendChild(stats.domElement);
+        /* Given a query in the form "lng, lat" or "lat, lng"
+        * returns the matching geographic coordinate(s) */
+        const coordinatesGeocoder = function (query) {
 
-            var light = new THREE.DirectionalLight(0xffffff);
-            light.position.set(0, -10, 10).normalize();
-            scene.add(light);
-            scene.add(new THREE.AmbientLight(0xffffff, 0.2));
-            addBar(scene, prop, ele);
-
-        };
-        threeLayer.addTo(map);
-    }
-
-    function addBar(scene, prop, ele) {
-        bars = [], selectMesh = [];
-        material = new THREE.MeshLambertMaterial({ color: 'green', transparent: true, opacity: 1 });
-        highlightmaterial = new THREE.MeshBasicMaterial({ color: 'blue', transparent: true });
-
-        let data = "";
-        var json = JSON.parse(prop);
-        for (var i = 0; i < json.features.length; i++) {
-            data = json.features.slice(0, Infinity).map(function (dataItem) {
-                dataItem = gcoord.transform(dataItem, gcoord.AMap, gcoord.WGS84);
-                return {
-                    coordinate: dataItem.geometry.coordinates,
-                    //height: dataItem.properties.Amount * 2,
-                    height: parseInt(dataItem.properties.Amount),
-                    value: dataItem.properties.Amount,
-                    city: dataItem.properties.City,
-                    zip: dataItem.properties.ZipCode,
-                    //height: Math.random() * 200,
-                    //value: Math.random() * 10000,
-                    color: dataItem.properties.Contract,
-                    topColor: '#fff'
-                }
-            });
-
-        }
-        const time = 'time';
-        console.time(time);
-        //                 const box = threeLayer.toBoxs(data, {}, material);
-        //                 bars.push(box);
-        data.forEach(dataItem => {
-            const bar = threeLayer.toBox(dataItem.coordinate, { height: dataItem.height, radius: 200 }, getMaterial(dataItem.color));
-
-            var tooltipTxt = 'Value: ' + dataItem.height;
-            var infoWindowTxt = 'City : ' + dataItem.city + '<br> ZipCode : ' + dataItem.zip + '<br> Value : ' + dataItem.height;
-
-            bar.setToolTip(tooltipTxt, {
-                showTimeout: 0,
-                eventsPropagation: true,
-                dx: 10
-            });
-
-            bar.setInfoWindow({
-                content: infoWindowTxt,
-                title: 'Info',
-                animationDuration: 0,
-                autoOpenOn: true
-            });
-
-            ['mouseout', 'mouseover'].forEach(function (eventType) {
-                bar.on(eventType, function (e) {
-                    // console.log(e.type, e);
-                    // console.log(this);
-                    if (e.type === 'mouseout') {
-                        this.setSymbol(getMaterial(dataItem.color));
-                    }
-                    if (e.type === 'mouseover') {
-                        this.setSymbol(highlightmaterial);
-                    }
-                });
-            });
-
-            bars.push(bar);
-        });
-        console.timeEnd(time);
-        threeLayer.addMesh(bars);
-
-        animation();
-        initGui(ele);
-    }
-
-    function getMaterial(color) {
-        if (!materials[color]) {
-            materials[color] = new THREE.MeshLambertMaterial({ color: color, transparent: true, opacity: 1 });
-        }
-        return materials[color];
-    }
-
-    function animation() {
-        // layer animation support Skipping frames
-        threeLayer._needsUpdate = !threeLayer._needsUpdate;
-        if (threeLayer._needsUpdate) {
-            threeLayer.renderScene();
-        }
-        stats.update();
-        requestAnimationFrame(animation);
-    }
-
-    function initGui(ele) {
-        var params = {
-            add: true,
-            color: material.color.getStyle(),
-            show: true,
-            opacity: material.opacity,
-            altitude: 0,
-            animateShow: animateShow
-        };
-
-        var gui = new dat.GUI();
-        gui.add(params, 'add').onChange(function () {
-            if (params.add) {
-                threeLayer.addMesh(bars);
-            } else {
-                threeLayer.removeMesh(bars);
+            const matches = query.match(
+                /^[ ]*(?:Lat: )?(-?\d+\.?\d*)[, ]+(?:Lng: )?(-?\d+\.?\d*)[ ]*$/i
+            );
+            if (!matches) {
+                return null;
             }
+
+            function coordinateFeature(lng, lat) {
+                return {
+                    center: [lng, lat],
+                    geometry: {
+                        type: 'Point',
+                        coordinates: [lng, lat]
+                    },
+                    place_name: 'Lat: ' + lat + ' Lng: ' + lng,
+                    place_type: ['coordinate'],
+                    properties: {},
+                    type: 'Feature'
+                };
+            }
+
+            const coord1 = Number(matches[1]);
+            const coord2 = Number(matches[2]);
+            const geocodes = [];
+
+            if (coord1 < -90 || coord1 > 90) {
+                // must be lng, lat
+                geocodes.push(coordinateFeature(coord1, coord2));
+            }
+
+            if (coord2 < -90 || coord2 > 90) {
+                // must be lat, lng
+                geocodes.push(coordinateFeature(coord2, coord1));
+            }
+
+            if (geocodes.length === 0) {
+                // else could be either lng, lat or lat, lng
+                geocodes.push(coordinateFeature(coord1, coord2));
+                geocodes.push(coordinateFeature(coord2, coord1));
+            }
+
+            return geocodes;
+        };
+
+        mapboxgl.accessToken = 'pk.eyJ1Ijoic3Nhbm9ja2kiLCJhIjoiY2t3NTB6bWdsMDJ6djMxbDViMTR5OG5waSJ9.Fby0ouQeXSNX8UUqzaoCmw';
+
+        var map = new mapboxgl.Map({
+            container: ele,
+            // style: 'mapbox://styles/mapbox/light-v9',
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: [-0.198465, 51.505538],
+            zoom: 14,
+            pitch: 40,
+            antialias: true
         });
-        gui.addColor(params, 'color').name('bar color').onChange(function () {
-            material.color.set(params.color);
-            bars.forEach(function (mesh) {
-                mesh.setSymbol(material);
+
+        map.addControl(
+            new MapboxGeocoder({
+                accessToken: mapboxgl.accessToken,
+                localGeocoder: coordinatesGeocoder,
+                zoom: 4,
+                placeholder: 'Enter location...',
+                mapboxgl: mapboxgl,
+                reverseGeocode: true
+            })
+        );
+
+        var dataUrl = 'https://raw.githubusercontent.com/s-sanocki/PUIC/master/Geomap/mapbox_circles_extrusion/heatmap-data.csv';
+
+        map.on('load', function () {
+
+            d3.csv(dataUrl).then(function (dataFetched) {
+      
+              var dataSource = {
+                "type": "FeatureCollection",
+                "features": []
+              }
+      
+              dataFetched.forEach(function (dataRow) {
+                dataSource.features.push(turf.point([dataRow.lng, dataRow.lat], { height: parseInt(dataRow.height), color: dataRow.color }));
+              })
+      
+              map.addSource("coordinates", {
+                "type": "geojson",
+                "data": dataSource
+              });
+      
             });
-        });
-        gui.add(params, 'opacity', 0, 1).onChange(function () {
-            material.opacity = params.opacity;
-            bars.forEach(function (mesh) {
-                mesh.setSymbol(material);
-            });
-        });
-        gui.add(params, 'show').onChange(function () {
-            bars.forEach(function (mesh) {
-                if (params.show) {
-                    mesh.show();
-                } else {
-                    mesh.hide();
+      
+            map.addLayer({
+              'id': 'extrusion',
+              'type': 'fill-extrusion',
+              "source": {
+                "type": "geojson",
+                "data": {
+                  "type": "FeatureCollection",
+                  "features": []
                 }
+              },
+              'paint': {
+                // 'fill-extrusion-color': '#00f',
+                'fill-extrusion-color': ['get', 'color'],
+                'fill-extrusion-height': ['get', 'height'],
+                'fill-extrusion-base': 0,
+                'fill-extrusion-opacity': 0.9
+              }
             });
-        });
-        gui.add(params, 'altitude', 0, 300).onChange(function () {
-            bars.forEach(function (mesh) {
-                mesh.setAltitude(params.altitude);
+      
+            //popup
+            var popup = new mapboxgl.Popup({
+              closeButton: false,
+              closeOnClick: false,
+              anchor: 'top',
             });
-        });
-        gui.add(params, 'animateShow');
-
-        $('.dg,.ac').css('display', 'none');
-
-        const rem = ele.childNodes[1];
-        rem.style.display = 'none';
-    }
-
-    function animateShow() {
-        bars.forEach(function (mesh) {
-            mesh.animateShow({
-                duration: 3000
-            });
-        });
+      
+            map.on("mousemove", function (e) {
+      
+              var coordinates = [e.lngLat.lng, e.lngLat.lat];
+      
+              var query = map.queryRenderedFeatures(e.point, {
+                layers: ["extrusion"]
+              });
+      
+              if (query.length) {
+                var properties = query[0].properties;
+                var html = "<h3>" + "Debt Amount: " + properties.height + "</h3>";
+                popup.setLngLat(coordinates)
+                  .setHTML(html)
+                  .addTo(map);
+              } else {
+                if (popup.isOpen() === true) {
+                  popup.remove();
+                }
+              }
+      
+            })
+      
+            map.on('sourcedata', function (e) {
+              if (e.sourceId !== 'coordinates') return
+              if (e.isSourceLoaded !== true) return
+      
+              var data = {
+                "type": "FeatureCollection",
+                "features": []
+              }
+      
+              e.source.data.features.forEach(function (f) {
+                var object = turf.centerOfMass(f)
+                var center = object.geometry.coordinates
+                var radius = 25;
+                var options = {
+                  steps: 6,
+                  units: 'meters',
+                  properties: f.properties
+                };
+                data.features.push(turf.circle(center, radius, options))
+              })
+              map.getSource('extrusion').setData(data);
+            })
+      
+          });
 
     }
 
